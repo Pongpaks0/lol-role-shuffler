843e6d7fe67766ddb972b1b1207f3947e42f2ac8
Pongsapak <san48192@gmail.com>
2024-10-03 19:28:34 +0700

    adjust color

--- scripts/genNameColorPair.js
+++ scripts/genNameColorPair.js
@@ -1,4 +1,10 @@
-const colors = ["#0072B2", "#D95F02", "#009E73", "#CC79A7", "#999999"];
+const colors = [
+  "hsl(201.57, 100%, 50%)",
+  "hsl(25.95, 98.17%, 40%)",
+  "hsl(163.67, 100%, 65%)",
+  "hsl(326.75, 44.86%, 80%)",
+  "hsl(0, 0%, 60%)",
+];
 
 export function genNameColorPair(names) {
   const res = new Map();
